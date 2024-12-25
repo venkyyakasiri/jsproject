035441b5edd50d232856f5732ff6fc927f0666eb
venkyyakasiri <yakasirivenkatesh@gmail.com>
2024-12-25 18:41:14 +0530

    Update pro.js

--- pro.js
+++ pro.js
@@ -14,7 +14,7 @@ recipeCloseBtn.addEventListener('click', () => {
 // get meal list that matches with the ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
